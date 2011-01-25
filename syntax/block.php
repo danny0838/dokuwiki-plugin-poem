87b54bf6df2ec08daa1f52e61b5127de7e01c5af
Danny Lin <danny0838@pchome.com.tw>
2011-01-25 11:15:09 +0800

    First release.

---
+++ syntax/block.php
@@ -0,0 +1,41 @@
+<?php
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_poem_block extends DokuWiki_Syntax_Plugin {
+
+    function getType() { return 'container'; }
+    function getPType() { return 'stack'; }
+    function getAllowedTypes() { return array('formatting', 'substition', 'disabled', 'poem'); }
+    function getSort() { return 20; }
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        $this->Lexer->addEntryPattern('<poem>\n?',$mode,'plugin_poem_block');
+    }
+
+    function postConnect() {
+        $this->Lexer->addExitPattern('</poem>','plugin_poem_block');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        if ($state==DOKU_LEXER_UNMATCHED) {
+            $handler->_addCall('cdata', array($match), $pos);
+        }
+        return false;
+    }
+
+    /**
+     * Create output
+     */
+    function render($format, &$renderer, $data) {}
+}
---
+++ syntax/eol.php
@@ -0,0 +1,40 @@
+<?php
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_poem_eol extends DokuWiki_Syntax_Plugin {
+
+    function getType() { return 'poem'; }
+    function getPType() { return 'normal'; }
+    function getSort() { return 369; }
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('(?:^[ \t]*)?\n',$mode,'plugin_poem_eol');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){return true;}
+
+    /**
+     * Create output
+     */
+    function render($format, &$renderer, $data) {
+        if($format == 'xhtml'){
+            $renderer->doc .= "<br/>\n";
+            return true;
+        } else if ($format == 'metadata') {
+            $renderer->doc .= "\n";
+            return true;
+        }
+        return false;
+    }
+}
